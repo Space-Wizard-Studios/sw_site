9b05d61b8b906c2af8b5c994696cb58064de09f1
Danilo Nobre <danilonobren@gmail.com>
2023-11-04 01:48:26 -0300

    remove netilfy, update packages, installing cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,38 +1,35 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
-
 import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
-
 import { SITE } from './src/config.mjs';
+import prefetch from "@astrojs/prefetch";
 
+import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
 export default defineConfig({
-	site: SITE.origin,
-	base: SITE.basePathname,
-	output: 'static',
-
-	integrations: [
-		react(),
-		mdx(),
-		tailwind({
-			config: {
-				applyBaseStyles: false,
-			},
-		}),
-		sitemap(),
-		partytown({
-			config: {
-				forward: ['dataLayer.push']
-			},
-		}),
-	],
-
-	vite: {
-		build: {
-			target: 'es2020',
-		},
-	},
+  site: SITE.origin,
+  base: SITE.basePathname,
+  output: 'static',
+  integrations: [
+    react(),
+    mdx(),
+    tailwind({
+      config: {
+        applyBaseStyles: false
+      }
+    }), sitemap(), partytown({
+      config: {
+        forward: ['dataLayer.push']
+      }
+    }), prefetch(), robotsTxt()],
+  vite: {
+    build: {
+      target: 'es2022'
+    },
+    types: ["vite/client"],
+  }
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
-/// <reference types="@astrojs/image/client" />
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
